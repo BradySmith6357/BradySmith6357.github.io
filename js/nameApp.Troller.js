4230ca6217d58360895301f740906d5c9b8f5586
BradySmith6357 <bsmit6357@gmail.com>
2016-06-02 10:29:13 -0600

    contact form working. background photos updated.

---
+++ js/nameApp.Troller.js
@@ -0,0 +1,20 @@
+angular.module('mainApp', [])
+angular.module('mainApp')
+	.controller('mainTroller', ['$scope', '$http', function ($scope, $http){
+
+		$scope.sendEmail = function(){
+			console.log($scope.email)
+			$http.post('api/send', $scope.email)
+				.then(function(returnData){
+					console.log(returnData)
+					if(returnData.status !== 200) {
+						alert("Sorry, there was an error. Please try again.")
+					} else {
+						alert("Your message has been sent!")
+					}
+				})
+			$scope.email = {}
+	};
+
+
+	}])
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var fs = require('fs')
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var nodemailer = require('nodemailer');
 var app = express();
+
 
 
 app.use(logger('dev'));
@@ -14,6 +16,38 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res){
 	res.sendFile('index.html', {root: './'})
 })
+
+// ***** NODEMAILER *****
+
+app.post('/api/send', function(req, res){
+	console.log(req.body)
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'bsmit6357@gmail.com',
+			pass: 'St33lers11'
+		} 
+	});
+
+	var mailOptions = {
+	    from: '<bradysmith6357@github.io>', 
+	    to: 'bsmit6357@gmail.com', 
+	    subject: 'Message from Your Website!', 
+	    text: 'You have a new email submitted through the your website. From: ' + req.body.name + 'Email: ' + req.body.address + 'Subject: ' + req.body.subject + 'Message: ' + req.body.message,
+	    html: '<p>You have a new email submitted through your website. Details Below</p><ul><li>From: ' + req.body.name + '</li><li>Email: ' + req.body.address + '</li><li>Subject: ' + req.body.subject + '</li><li>Message: ' + req.body.message + '</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(err, info){
+	    if(err){
+	        console.log(err);
+	        res.send(err)
+	    } else {
+	    	console.log('Message Sent: ' + info.response)
+	    	res.send(info)
+	    }
+	});
+});
+
 
 var port = 3000
 app.listen(port, function(){
