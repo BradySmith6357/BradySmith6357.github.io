cf1654a7645307211ea714f83cc23794a9f259b9
BradySmith6357 <bsmit6357@gmail.com>
2016-06-02 15:35:35 -0600

    nodemailer adjustments

--- server.js
+++ server.js
@@ -19,30 +19,29 @@ app.get('/', function(req, res){
 
 // ***** NODEMAILER *****
 
-var smtpTransport = nodemailer.createTransport("SMTP", {
-  service: "Gmail",
-  auth: {
-    XOAuth2: {
-      user: "bsmit6357@gmail.com", // Your gmail address.
-                                            // Not @developer.gserviceaccount.com
-      clientId: "855490167743-t3hum7ki3108tnuoqgrg2l81tk2ouire.apps.googleusercontent.com",
-      clientSecret: "CSji2-61GjcKTazvIIcBmB95",
-      refreshToken: "1/Pd9ESLMe2APu6OfAlrE4uQj1Q0AjfQoOE2es3C-Dz7U"
-    }
-  }
-});
+app.post('/api/send', function(req, res){
+	console.log(req.body)
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			XOAuth2: {
+				user: "bsmit6357@gmail.com",
+				clientId: "855490167743-t3hum7ki3108tnuoqgrg2l81tk2ouire.apps.googleusercontent.com",
+				clientSecret: "CSji2-61GjcKTazvIIcBmB95",
+				refreshToken: "1/Pd9ESLMe2APu6OfAlrE4uQj1Q0AjfQoOE2es3C-Dz7U"
+			}
+		} 
+	});
 
-var mailOptions = {
-  from: '<bsmit6357@gmail.com>', 
-	to: 'bsmit6357@gmail.com', 
-	subject: 'Message from Your Website!', 
-	text: 'You have a new email submitted through the your website. From: ' + req.body.name + 'Email: ' + req.body.address + 'Subject: ' + req.body.subject + 'Message: ' + req.body.message,
-	html: '<p>You have a new email submitted through your website. Details Below</p><ul><li>From: ' + req.body.name + '</li><li>Email: ' + req.body.address + '</li><li>Subject: ' + req.body.subject + '</li><li>Message: ' + req.body.message + '</li></ul>'
-  	generateTextFromHTML: true
-};
+	var mailOptions = {
+	    from: '<bsmit6357@gmail.com>', 
+	    to: 'bsmit6357@gmail.com', 
+	    subject: 'Message from Your Website!', 
+	    text: 'You have a new email submitted through the your website. From: ' + req.body.name + 'Email: ' + req.body.address + 'Subject: ' + req.body.subject + 'Message: ' + req.body.message,
+	    html: '<p>You have a new email submitted through your website. Details Below</p><ul><li>From: ' + req.body.name + '</li><li>Email: ' + req.body.address + '</li><li>Subject: ' + req.body.subject + '</li><li>Message: ' + req.body.message + '</li></ul>'
+	};
 
-
-	smtpTransporter.sendMail(mailOptions, function(err, info){
+	transporter.sendMail(mailOptions, function(err, info){
 	    if(err){
 	        console.log(err);
 	        res.send(err)
@@ -58,22 +57,3 @@ var port = 3000
 app.listen(port, function(){
 	console.log("Running on port:", port)
 })
-
-// ****** NODEMAILER WITHOUT OAUTH *******
-// app.post('/api/send', function(req, res){
-// 	console.log(req.body)
-// 	var transporter = nodemailer.createTransport({
-// 		service: 'Gmail',
-// 		auth: {
-// 			user: 'bsmit6357@gmail.com',
-// 			pass: 'St33lers11'
-// 		} 
-// 	});
-
-// 	var mailOptions = {
-// 	    from: '<bsmit6357@gmail.com>', 
-// 	    to: 'bsmit6357@gmail.com', 
-// 	    subject: 'Message from Your Website!', 
-// 	    text: 'You have a new email submitted through the your website. From: ' + req.body.name + 'Email: ' + req.body.address + 'Subject: ' + req.body.subject + 'Message: ' + req.body.message,
-// 	    html: '<p>You have a new email submitted through your website. Details Below</p><ul><li>From: ' + req.body.name + '</li><li>Email: ' + req.body.address + '</li><li>Subject: ' + req.body.subject + '</li><li>Message: ' + req.body.message + '</li></ul>'
-// 	};
